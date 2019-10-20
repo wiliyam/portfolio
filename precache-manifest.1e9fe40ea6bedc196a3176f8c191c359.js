7e84e4e4c305b03a74207bf5b8b1bfc96e64c503
wiliyam <wiliyambhadani@gmail.com>
2019-10-20 19:25:39 +0530

    Updates

---
+++ precache-manifest.1e9fe40ea6bedc196a3176f8c191c359.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8afe154bcb9d00afc4c8f4872e16cc16",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f3b835a9386a564fba8c",
+    "url": "/portfolio/static/css/main.122ae523.chunk.css"
+  },
+  {
+    "revision": "0bab3904e6df782dfcd3",
+    "url": "/portfolio/static/js/2.40e9b666.chunk.js"
+  },
+  {
+    "revision": "f3b835a9386a564fba8c",
+    "url": "/portfolio/static/js/main.2ab219bb.chunk.js"
+  },
+  {
+    "revision": "ad950fac83bd4592c8eb",
+    "url": "/portfolio/static/js/runtime-main.34e9219c.js"
+  }
+]);
